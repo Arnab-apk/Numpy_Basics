68442fca05a5ec7004c659aa94531429e07b1be7
Mr.Invictus <arnabmandal261@gmail.com>
2025-10-23 23:47:06 +0530

    d

---
+++ Numpy_array_prop/shape.py
@@ -0,0 +1,5 @@
+#when working with mul dim data it gives the dimension
+import numpy as np
+arr_2d=np.array([[1,2,3],[3,4,5]])
+print(arr_2d.shape)
+
---
+++ Numpy_array_prop/size.py
@@ -0,0 +1,4 @@
+#it gives the total number of elements in an array
+import numpy as np
+arr_2d=np.array([[1,2,3],[3,4,5]])
+print(arr_2d.size)
