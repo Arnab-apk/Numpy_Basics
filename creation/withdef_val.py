6343499b28e1ce6b1cb7154b1de8b7e8ab9e05d8
Mr.Invictus <arnabmandal261@gmail.com>
2025-10-23 10:22:14 +0530

    d

---
+++ creation/withdef_val.py
@@ -0,0 +1,14 @@
+#np.zeros(shape)
+#for example 3
+import numpy as np
+zeros_array=np.zeros(3)
+print(zeros_array)
+
+#np.ones(shape)
+ones_array=np.ones([2,3])
+print(ones_array)
+
+#full(shape,values)
+
+filled_array=np.full([2,2],7)
+print(filled_array)
