2f6df63c80ac86331aaa938797a521a5d4a587b1
Mr.Invictus <arnabmandal261@gmail.com>
2025-10-23 23:38:00 +0530

    d

--- creation/sequence_cre.py
+++ creation/sequence_cre.py
@@ -1,6 +1,8 @@
 #creating sequences of nummbes in numpy
 #arange(start,stop,step)
-
+#t returns in teh form of numpy array 
 import numpy as np
 arr=np.arange(1,10,2)
-print(arr)
+print(f"{arr}\n")
+arr2=np.arange(10,1,-1)
+print(arr2)
