4340481b41ef41be7ebf6c414d665bf535ef215c
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-10-26 13:39:03 +0530

    d

--- Advance_part/append.py
+++ Advance_part/append.py
@@ -1,1 +1,9 @@
 import numpy as np
+arr=np.array([[1,2],[3,4]])
+print(f"{arr}\n")
+#append a new row
+new_arr=np.append(arr,[[5,6]],axis=0)
+print(f"{new_arr}\n")
+#append a new column
+new_arr=np.append(arr,[[5],[6]],axis=1)
+print(f"{new_arr}\n")
