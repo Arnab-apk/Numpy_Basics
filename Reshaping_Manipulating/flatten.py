5b6a50b618c5907a9fe022108bf559ad09856ce1
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-10-26 12:29:12 +0530

    d

---
+++ Reshaping_Manipulating/flatten.py
@@ -0,0 +1,8 @@
+#used in cases when a multidimentioninal array need sto be converted to a 1d array
+#.ravel->return a view(original array is affected)
+#.flatten ->copy(original array is not affected)
+import numpy as np
+arr=np.array([[1,2,3,4,5],[10,20,30,40,50]])
+print(arr.ravel())
+print(arr.flatten())
+
--- Reshaping_Manipulating/reshape.py
+++ Reshaping_Manipulating/reshape.py
@@ -1,6 +1,8 @@
 #reshaping does not create a copy it only returns a view of teh numpy matrix
+
 import numpy as np
 arr=np.array([1,2,3,4,5,6])
 reshaped_arr=arr.reshape(2,3)
 print(reshaped_arr)
+
 
