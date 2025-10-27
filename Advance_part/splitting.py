84a6b93c416b5766b1767c80645fb6f645bdcee4
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-10-27 11:32:26 +0530

    d

--- Advance_part/splitting.py
+++ Advance_part/splitting.py
@@ -1,3 +1,12 @@
+"""
+np.slpit(array,indices_or_sections,axis=0)
+splits an array into multiple sub-arrays along a specified axis.
+np.hsplit() - splits along the horizontal axis (columns).
+np.vsplit() - splits along the vertical axis (rows).
+
+"""
 import numpy as np
 arr1=np.array([[1,2,3],[4,5,6]])
 arr2=np.array([[7,8,9],[10,11,12]])
+print(np.split(arr1,2,axis=0)) #splits into 2 arrays along rows
+print(np.split(arr2,2,axis=0)) #splits into 2 arrays along rows
