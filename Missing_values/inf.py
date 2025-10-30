032e121166a4a1315b6e3e56ccc2443ed4ab32c3
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-10-30 19:40:03 +0530

    d

--- Missing_values/inf.py
+++ Missing_values/inf.py
@@ -2,3 +2,7 @@ import numpy as np
 arr=np.array([1, 2, 3,np.inf, 4, -np.inf, 5])
 print(np.isinf(arr))
 print("Number of infinite values:", np.isinf(arr).sum())
+
+#replace inf with large finite number
+cleaned_arr=np.nan_to_num(arr, posinf=1010, neginf=-1010)
+print(cleaned_arr)
